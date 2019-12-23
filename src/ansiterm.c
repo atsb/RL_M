2ace76d428d813ad26f452575e74b4ed44d30d08
Gibbon <44937323+atsb@users.noreply.github.com>
2019-12-23 15:40:47 +0100

    adding back a tiny amount of color
    this is just for the player '@' sign so it's easy to see during game-play, especially as a sapphire has the same character.

--- src/ansiterm.c
+++ src/ansiterm.c
@@ -127,6 +127,9 @@ ansiterm_init (void)
 	nonl();
 	intrflush(stdscr, FALSE);
 	keypad(stdscr, TRUE);
+	start_color();
+	use_default_colors();
+	init_pair(1, COLOR_WHITE, COLOR_RED);
 	curs_set(0);
 	refresh();
 

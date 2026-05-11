0ea40cdb944b743ae33edd139c817948dbd84df3
atsb <44937323+atsb@users.noreply.github.com>
2026-05-11 21:42:53 +0200

    More gcc fixes
    I'm doing this on MSVC too

--- main.c
+++ main.c
@@ -313,7 +313,7 @@ main (int argc, char *argv[])
 		  /* redraw only puddles for performance reasons (low cpu usage) */
 		  for (int yy = 0; yy < MAXY; yy++)
 			  for (int xx = 0; xx < MAXX; xx++)
-				  if (item[xx][yy] == OWATER && OSHOREWATER)
+				  if (item[xx][yy] == OWATER || item[xx][yy] == OSHOREWATER)
 					  show1cell(xx, yy);
 
 		  refresh();

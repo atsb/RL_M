e439e9ec8d76ed3a88e94fe791b13b729cf938e6
atsb <44937323+atsb@users.noreply.github.com>
2026-05-21 10:37:06 +0200

    Testing some exotic platforms for portability

---
+++ mk/objects.mk
@@ -0,0 +1,29 @@
+# Object list for all builds of Larn
+# This file is included by every Makefile.<arch>
+
+OBJS = \
+    action.o \
+    bill.o \
+    config.o \
+    create.o \
+    data.o \
+    diag.o \
+    display.o \
+    fortune.o \
+    global.o \
+    help.o \
+    inventory.o \
+    io.o \
+    main.o \
+    monster.o \
+    moreobj.o \
+    movem.o \
+    nap.o \
+    object.o \
+    regen.o \
+    savelev.o \
+    scores.o \
+    spells.o \
+    spheres.o \
+    store.o \
+    tok.o
